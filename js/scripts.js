5f1f9415ddd8af3c5f9252ecce262613569064da
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-01-26 16:30:56 -0800

    Add two ojct to get total score
    Co-Authored-By Jake Haley <jakechaley@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,54 @@
-function Player(name) {
-  this.name = name
-  this.diceNumber=0;
+function Game()
+{
+  this.players = {};
   this.totalScore=0;
-  this.tempScore=0;
   console.log(this.name);
 }
 
-
+function Player(name) {
+  this.name = name
+  this.tempScore=0;
+  this.diceNumber=0;
+  
+}
 
 Player.prototype.roll=function(){
  this.diceNumber= Math.floor(Math.random() * 6) + 1;
+ console.log("object" +this.player);
  console.log("roll "+this.diceNumber);
  this.tempScore = this.diceNumber;
- this.total(this.tempScore);
+ //this.tempScore += this.diceNumber;
+ console.log("tempScore" + this.tempScore)
+ //this.total(this.tempScore);
 };
 
 
-
-
-
-Player.prototype.total=function(tempScore){
-  if(this.totalScore==="100"){
-    //  //we need to call the second player object
-}
-if(this.tempScore==="1"){
-  this.totalScore=0;
-  this.tempScore = 0;
+function calculateTotalScore(playerObj){
+  //add the score of each dice roll of the player1
+  //if player rolls 1 no points are added that round
+  console.log(playerObj);
+  console.log(playerObj.tempScore);
+ game.totalScore += parseInt(playerObj.tempScore);
+ console.log("calculateTotal"+game.totalScore)
 
 }
- 
-    console.log("temp "+ this.tempScore);
-   this.totalScore += this.tempScore;
-   console.log("total "+ this.totalScore);
-   return this.totalScore
- 
-}
+
 Player.prototype.hold= function(){
   //we need to call the second player object
 }
+let game= new Game();
+$(document).ready(function() {
+  $("form#player").submit(function() {
+    event.preventDefault();
+    console.log("inside submit");
+    let player= new Player("P1");
+    player.roll();
+   calculateTotalScore(player);
+    
 
-let player1= new Player("P1");
-player1.roll();
-player1.roll();
+  });
+});
+
+
 //player1.total();
 
